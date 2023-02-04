bb37f992f1cba7c9ff1721070eb6731330818cdc
Atom <albaraa456@gmail.com>
2023-02-04 20:19:29 +0300

    everything

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./theme/modules/**/*.{js,ts,jsx,tsx,html,twig}",
-    "./theme/common/**/*.{js,ts,jsx,tsx,html,twig}",
-    "./theme/templates/**/*.{js,ts,jsx,tsx,html,twig}",
+    "./theme/**/*.{js,ts,jsx,tsx,html,twig}",
     "./theme/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
