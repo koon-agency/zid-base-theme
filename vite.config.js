9ae88b9ad9618012f23bc025df3a9bbb46231995
Atom <albaraa456@gmail.com>
2023-04-03 02:27:19 +0300

    keep assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     publicDir: 'theme/assets',
     mode: 'production',
+    emptyOutDir: false,
     build: {
         minify: true,
         lib: {
